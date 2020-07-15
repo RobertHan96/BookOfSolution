8ad67d9ae387da0dd120e8a7131d48d0ad04b2c1
RobertHan96 <yshan4329@gmail.com>
2020-07-15 11:54:36 +0900

    다크모드 대응

--- BookOfSolution/Controller/MainViewController.swift
+++ BookOfSolution/Controller/MainViewController.swift
@@ -5,18 +5,26 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setGestureRecogniezer()
+    }
+    
+    func setupUI() {
         titleLabel.text = "title".localized
         titleLabel.adjustsFontSizeToFitWidth = true
-        let upSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction(_:)))
+    }
+    
+    func setGestureRecogniezer() {
+        let upSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(goSolutionViewAction(_:)))
         upSwipeGesture.direction = .left
         view.addGestureRecognizer(upSwipeGesture)
     }
     
-    @IBAction func swipeAction (_ sender : UISwipeGestureRecognizer) {
-//        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    @IBAction func goSolutionViewAction (_ sender : UISwipeGestureRecognizer) {
         guard let wisdomVC =
         self.storyboard?.instantiateViewController(withIdentifier: "wisdomVC") else {return}
         wisdomVC.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        wisdomVC.modalPresentationStyle = .fullScreen
         if sender.direction == .left {
             self.present(wisdomVC, animated: true, completion: nil)
         }
--- BookOfSolution/Controller/SolutionViewController.swift
+++ BookOfSolution/Controller/SolutionViewController.swift
@@ -2,20 +2,35 @@ import UIKit
 
 class SolutionViewController: UIViewController {
     @IBOutlet weak var wisdomLabel: UILabel!
+    let randomIndex = makeRandomNum()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let randomIndex = makeRandomNum()
-        wisdomLabel.adjustsFontSizeToFitWidth = true
-        wisdomLabel.text = widoms[randomIndex]
+        setupUI()
+        setGestureRecogniezer()
     }
     
-    // 해결문구가 서서히 나타나도록 애니메이션을 적용한 부분
     override func viewWillAppear(_ animated: Bool) {
         UIView.animate(withDuration: 3) {
             self.wisdomLabel.alpha = 0.0
             self.wisdomLabel.alpha = 1.0
-            self.wisdomLabel.textColor = .darkText
+        }
+    }
+    
+    func setupUI() {
+        wisdomLabel.adjustsFontSizeToFitWidth = true
+        wisdomLabel.text = widoms[randomIndex]
+    }
+    
+    func setGestureRecogniezer() {
+        let upSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(goBackAction(_:)))
+        upSwipeGesture.direction = .right
+        view.addGestureRecognizer(upSwipeGesture)
+    }
+    
+    @IBAction func goBackAction (_ sender : UISwipeGestureRecognizer) {
+        if sender.direction == .right {
+            self.dismiss(animated: true, completion: nil)
         }
     }
    
